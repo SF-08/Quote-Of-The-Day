3a476b6fb9e42643116675abcf39781e91d152d3
Scott Fuller <shrgf1234@gmail.com>
2025-04-22 14:20:47 +0100

    cleared love command - finding reliable API to provide the quotes.

--- lib/cogs/love.py
+++ lib/cogs/love.py
@@ -1,17 +1,5 @@
-import os
-from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+
 import random
 from aiohttp import requests
-
-load_dotenv()
-key = os.getenv('API_KEY')
-
-class love(commands.Cog):
-    api_url = 'https://api.api-ninjas.com/v1/quotes'
-    response = requests.get(api_url, headers={'X-Api-Key': 'API_KEY'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
-    else:
-        print("Uh Oh! I have an error.", response.status_code, response.text)
