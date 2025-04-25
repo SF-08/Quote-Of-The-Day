ce3532a0fb93d9277c2da58626a2b23a9e87e6d6
Scott Fuller <shrgf1234@gmail.com>
2025-04-25 20:54:28 +0100

    Added Events such as a bot status loop which changes the bot status every 20 seconds

---
+++ cogs/events.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
+
+#Bot Status Cycle
+
+bot_status = cycle(['Quoting Quotes.', 'Knowledge is power.', 'https://github.com/SF-08'])
+
+class events(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
+
+    #Status Loop
+
+    @tasks.loop(seconds=20, count=None, reconnect=True)
+    async def status_loop(self):
+        await self.client.change_presence(status=discord.Status.online, activity=discord.Game(next(bot_status)))
+        
